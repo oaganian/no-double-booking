977793ca6f95b6ac127c5a1bcf70bcfe4f5cd1d5
Oganes Aganyan <o.aganian@hh.ru>
2026-07-15 15:29:31 +0300

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,45 @@
+// @ts-check
+import eslint from '@eslint/js';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    ignores: [
+      'eslint.config.mjs',
+      'dist/',
+      'node_modules/',
+      'coverage/',
+      '*.json',
+    ],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  eslintPluginPrettierRecommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      sourceType: 'module',
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-unsafe-assignment': 'error',
+      '@typescript-eslint/no-floating-promises': 'error',
+      '@typescript-eslint/no-unsafe-argument': 'error',
+      '@typescript-eslint/no-unsafe-return': 'error',
+      '@typescript-eslint/no-unsafe-call': 'error',
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+    },
+  },
+);
